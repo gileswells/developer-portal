f009f8ffa96584be2649c0a1d56f64f4965a1f62
Alexander James Valencia <alexanderjamesvalencia@AlexanderValencia-Macbook.attlocal.net>
2020-10-26 12:48:11 -0500

    Fix timing issues

--- src/containers/documentation/SwaggerDocs.tsx
+++ src/containers/documentation/SwaggerDocs.tsx
@@ -1,9 +1,10 @@
 import * as Sentry from '@sentry/browser';
+import { History } from 'history';
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useLocation } from 'react-router-dom';
-import { usePrevious } from 'src/hooks/previous/Previous';
 import SwaggerUI from 'swagger-ui';
+import { usePrevious } from '../../hooks';
 import * as actions from '../../actions';
 import { APIDocSource } from '../../apiDefs/schema';
 import { getDocURL, getVersion, getVersionNumber } from '../../reducers/api-versioning';
@@ -25,6 +26,52 @@ export interface VersionInfo {
   internal_only: boolean;
 }
 
+const getMetadata = async (metadataUrl?: string): Promise<APIMetadata | null> => {
+  if (!metadataUrl) {
+    return null;
+  }
+  try {
+    const request = new Request(`${metadataUrl}`, {
+      method: 'GET',
+    });
+    const response = await fetch(request);
+    return response.json() as Promise<APIMetadata>;
+  } catch (error) {
+    Sentry.captureException(error);
+    return null;
+  }
+};
+
+const setSearchParam = (history: History, queryString: string, version: string) => {
+  const params = new URLSearchParams(queryString);
+  if (params.get('version') !== version) {
+    params.set('version', version);
+    history.push(`${history.location.pathname}?${params.toString()}`);
+  }
+};
+
+const renderSwaggerUI = (
+  dispatch: React.Dispatch<actions.SetRequestedAPIVersion>,
+  docUrl: string,
+  versionNumber: string,
+  metadata: APIMetadata | null,
+) => {
+  const handleVersionChange = (requestedVersion: string) => {
+    dispatch(actions.setRequstedApiVersion(requestedVersion));
+  };
+  if (document.getElementById('swagger-ui') && docUrl.length !== 0) {
+    const plugins = SwaggerPlugins(handleVersionChange);
+    const ui: System = SwaggerUI({
+      dom_id: '#swagger-ui',
+      layout: 'ExtendedLayout',
+      plugins: [plugins],
+      url: docUrl,
+    }) as System;
+    ui.versionActions.setApiVersion(versionNumber);
+    ui.versionActions.setApiMetadata(metadata as APIMetadata);
+  }
+};
+
 const SwaggerDocs = (props: SwaggerDocsProps): JSX.Element => {
   const dispatch: React.Dispatch<
     actions.SetRequestedAPIVersion | actions.SetInitialVersioning
@@ -37,92 +84,46 @@ const SwaggerDocs = (props: SwaggerDocsProps): JSX.Element => {
 
   const history = useHistory();
   const location = useLocation();
-  const prevApiName = usePrevious(props.apiName);
+
+  const { apiName } = props;
+
+  const prevApiName = usePrevious(apiName);
   const prevVersion = usePrevious(version);
 
-  const setSearchParam = React.useCallback(() => {
-    const params = new URLSearchParams(location.search);
-    if (params.get('version') !== version) {
-      console.log('setSearchParam()');
-      params.set('version', version);
-      history.push(`${history.location.pathname}?${params.toString()}`);
-    }
-  }, [history, location.search, version]);
+  const { openApiUrl, metadataUrl } = props.docSource;
 
-  const setMetadataAndDocUrl = React.useCallback(async () => {
-    const { openApiUrl, metadataUrl } = props.docSource;
-    const currentMetadata = (await getMetadata(metadataUrl)) as string | undefined;
+  const setMetadataAndDocUrl = async () => {
+    const currentMetadata = await getMetadata(metadataUrl);
 
     dispatch(actions.setInitialVersioning(openApiUrl, currentMetadata));
-  }, [dispatch, props.docSource]);
-
-  const getMetadata = async (metadataUrl?: string): Promise<any> => {
-    if (!metadataUrl) {
-      return null;
-    }
-    try {
-      const request = new Request(`${metadataUrl}`, {
-        method: 'GET',
-      });
-      const response = await fetch(request);
-      return response.json();
-    } catch (error) {
-      Sentry.captureException(error);
-      return null;
-    }
   };
 
-  const renderSwaggerUI = React.useCallback(() => {
-    const handleVersionChange = (currentVersion: string) => {
-      dispatch(actions.setRequstedApiVersion(currentVersion));
-      setSearchParam();
-    };
-    if (document.getElementById('swagger-ui') && docUrl.length !== 0) {
-      console.log('renderSwaggerUI()');
-      const plugins = SwaggerPlugins(handleVersionChange);
-      const ui: System = SwaggerUI({
-        dom_id: '#swagger-ui',
-        layout: 'ExtendedLayout',
-        plugins: [plugins],
-        url: docUrl,
-      }) as System;
-      ui.versionActions.setApiVersion(versionNumber);
-      ui.versionActions.setApiMetadata(metadata as APIMetadata);
-    }
-  }, [dispatch, docUrl, metadata, setSearchParam, versionNumber]);
-
-  const setupAndRenderSwaggerUI = React.useCallback(async () => {
-    await setMetadataAndDocUrl();
-    setSearchParam();
-    renderSwaggerUI();
-  }, [renderSwaggerUI, setMetadataAndDocUrl, setSearchParam]);
+  if (prevApiName !== apiName) {
+    void setMetadataAndDocUrl();
+  }
 
   /**
-   * componentDidMount()
+   * Clear state on unmount
    */
-  React.useLayoutEffect(() => {
-    void setupAndRenderSwaggerUI();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  React.useEffect(
+    () => () => {
+      dispatch(actions.setInitialVersioning('', null));
+    },
+    [], // eslint-disable-line react-hooks/exhaustive-deps
+  );
 
-  /**
-   * componentDidUpdate()
-   */
-  React.useLayoutEffect(() => {
-    if (prevApiName !== props.apiName) {
-      void setupAndRenderSwaggerUI();
-    } else if (prevVersion !== version) {
-      setSearchParam();
-      renderSwaggerUI();
+  React.useEffect(() => {
+    let navigating = false;
+
+    if (prevVersion !== version) {
+      navigating = true;
+      setSearchParam(history, location.search, version);
     }
-  }, [
-    prevApiName,
-    prevVersion,
-    props.apiName,
-    renderSwaggerUI,
-    setSearchParam,
-    setupAndRenderSwaggerUI,
-    version,
-  ]); // eslint-disable-line react-hooks/exhaustive-deps
+
+    if (!navigating) {
+      renderSwaggerUI(dispatch, docUrl, versionNumber, metadata);
+    }
+  }, [dispatch, docUrl, history, location.search, prevVersion, version, versionNumber, metadata]);
 
   const { apiIntro } = props.docSource;
 
