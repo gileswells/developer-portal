5184d3fda315b8b26f9fa4a559072f9bbc9acc1a
ediiotero <eddie.otero@oddball.io>
2021-02-01 10:20:58 -0500

    update hash links, moved mdx table into parent mdx files

--- src/types/md.d.ts
+++ src/types/md.d.ts
@@ -3,11 +3,8 @@ declare module '*.mdx' {
    * This type definition isn't entirely accurate. The underlying component would be happy to
    * accept more attributes than this type signature will allow. However, the precise definition
    * of those extra attributes is defined by the markdown-component-loader at runtime based on
-   * configuration data. This constrained type signature is used here as a statement of our
-   * preference to limit our use the component loader to its basic functionality.
-   */
-  /**
-   * adding 'propObj' so the mdx files are open to any props that are passed
+   * configuration data.
+   * Adding 'propObj' so the mdx files are open to any props that are passed
    */
   interface MarkdownComponentProps {
     className?: string;
