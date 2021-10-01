f957894fdf9438b14cac8558e3aa441e9be0ffca
Giles Wells <giles@mediagearhead.com>
2021-10-01 12:10:48 -0400

    Also install Source Sans Pro

--- src/visualRegressionTest.ts
+++ src/visualRegressionTest.ts
@@ -14,7 +14,7 @@ const checkScreenshots = async (page: Page, selector: string): Promise<void> => 
   for (const viewport of viewports) {
     await page.setViewport(viewport);
     // eslint-disable-next-line no-promise-executor-return
-    await new Promise(resolve => setTimeout(resolve, 500));
+    await new Promise(resolve => setTimeout(resolve, 200));
     const content = await page.$(selector);
     if (content) {
       const screenshot = await content.screenshot({});
@@ -28,22 +28,6 @@ const checkScreenshots = async (page: Page, selector: string): Promise<void> => 
 const paths = testPaths.filter(path => path !== '/');
 
 describe('Visual regression test', () => {
-  it.each(paths)('renders %s properly', async (path: string) => {
-    // Mock swagger requests on docs pages so those pages aren't blank
-    if (/^\/explore\/[^\/]+\/docs/.test(path)) {
-      await page.setRequestInterception(true);
-      page.removeAllListeners('request');
-      page.on('request', mockSwagger);
-    }
-
-    await page.goto(`${puppeteerHost}${path}`, { waitUntil: 'networkidle0' });
-    // Hide any videos that may be on the page
-    await page.evaluate(
-      'document.querySelectorAll("iframe").forEach((e) => { e.style="visibility: hidden;" });',
-    );
-    await checkScreenshots(page, 'main');
-  });
-
   it('renders the homepage properly', async () => {
     // Set unlimited timeout on first request, since it may timeout while webpack is compiling.
     await page.goto(`${puppeteerHost}`, { timeout: 0, waitUntil: 'networkidle0' });
@@ -58,5 +42,21 @@ describe('Visual regression test', () => {
   it('renders the footer properly', async () => {
     await page.goto(`${puppeteerHost}`, { waitUntil: 'networkidle0' });
     await checkScreenshots(page, 'footer');
+  });
+
+  it.each(paths)('renders %s properly', async (path: string) => {
+    // Mock swagger requests on docs pages so those pages aren't blank
+    if (/^\/explore\/[^\/]+\/docs/.test(path)) {
+      await page.setRequestInterception(true);
+      page.removeAllListeners('request');
+      page.on('request', mockSwagger);
+    }
+
+    await page.goto(`${puppeteerHost}${path}`, { waitUntil: 'networkidle0' });
+    // Hide any videos that may be on the page
+    await page.evaluate(
+      'document.querySelectorAll("iframe").forEach((e) => { e.style="visibility: hidden;" });',
+    );
+    await checkScreenshots(page, 'main');
   });
 });
