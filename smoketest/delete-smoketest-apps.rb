342662a77131e1120eeef9041ce77720a8a8a654
Giles Wells <giles@mediagearhead.com>
2022-09-07 15:53:35 -0400

    Better gem loading?

--- smoketest/delete-smoketest-apps.rb
+++ smoketest/delete-smoketest-apps.rb
@@ -1,15 +1,5 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'vendor', 'bundle', 'ruby', '3.1.0', 'gems', 'addressable-2.7.0', 'lib'))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'vendor', 'bundle', 'ruby', '3.1.0', 'gems', 'faraday-1.4.1', 'lib'))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'vendor', 'bundle', 'ruby', '3.1.0', 'gems', 'faraday-excon-1.1.0', 'lib'))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'vendor', 'bundle', 'ruby', '3.1.0', 'gems', 'faraday-net_http-1.0.1', 'lib'))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'vendor', 'bundle', 'ruby', '3.1.0', 'gems', 'faraday-net_http_persistent-1.1.0', 'lib'))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'vendor', 'bundle', 'ruby', '3.1.0', 'gems', 'multipart-post-2.1.1', 'lib'))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'vendor', 'bundle', 'ruby', '3.1.0', 'gems', 'oktakit-0.3.3', 'lib'))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'vendor', 'bundle', 'ruby', '3.1.0', 'gems', 'public_suffix-4.0.6', 'lib'))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'vendor', 'bundle', 'ruby', '3.1.0', 'gems', 'sawyer-0.8.2', 'lib'))
-p $LOAD_PATH
-
-require 'oktakit'
+require 'rubygems'
+require 'bundler/setup'
 
 id = ARGV[0]
 
