b05d8aaeee845a7c957604f5248b64d8bff985d5
Andrés Rivera Toro <andres.rivera.toro@gmail.com>
2023-12-21 12:13:35 -0500

    chore: update text for about page (#1428)

--- src/containers/about/platformMetrics.ts
+++ src/containers/about/platformMetrics.ts
@@ -10,14 +10,14 @@ export const platformMetrics: PlatformMetric[] = [
   },
   {
     callToAction: 'Learn about production access',
-    content: 'We have over 90 consumers in production, with more on the way.',
-    title: '90+ Consumers',
+    content: 'We have over 130 consumers in production, with more on the way.',
+    title: '130+ Consumers',
     url: CONSUMER_PATH,
   },
   {
     callToAction: 'See all the APIs we offer',
-    content: 'Over 13 APIs added since we started, and we’re still growing. ',
-    title: '13+ APIs',
+    content: 'Over 20 APIs added since we started, and we’re still growing. ',
+    title: '20+ APIs',
     url: '/explore',
   },
 ];
